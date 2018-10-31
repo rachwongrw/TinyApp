e4b1b61fe7a9d2202bc66a73df43bf85c7a988b7
Rachel Wong <rwrachelwong@gmail.com>
2018-10-31 20:53:53 +0000

    Add Delete function

--- express_server.js
+++ express_server.js
@@ -48,6 +48,10 @@ app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+    delete urlDatabase[req.params.id];
+    res.redirect('/urls');
+});
 
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
